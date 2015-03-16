73b4f4210a4ecc1f0d2414273910ba1ae16eceaf
Matthew-Dove <mymailenator+github@gmail.com>
2015-03-16 18:38:38 +1100

    Adding polyfill for IE.

--- src/inject.js
+++ src/inject.js
@@ -1,4 +1,33 @@
 (function () {
+    /* Add text/html support for browsers that don't have it on the DOMParser object. */
+    if (typeof DOMParser !== 'undefined') {
+        (function (DOMParser) {
+            var DOMParser_proto = DOMParser.prototype
+            var real_parseFromString = DOMParser_proto.parseFromString;
+            
+            try {
+                if ((new DOMParser).parseFromString('', 'text/html')) {
+                    return;
+                }
+            } catch (e) { }
+            
+            DOMParser_proto.parseFromString = function (markup, type) {
+                if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+                    var doc = document.implementation.createHTMLDocument('');
+                    if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+                        doc.documentElement.innerHTML = markup;
+                    }
+                    else {
+                        doc.body.innerHTML = markup;
+                    }
+                    return doc;
+                } else {
+                    return real_parseFromString.apply(this, arguments);
+                }
+            };
+        }(DOMParser));
+    }
+    
     /* Build the url for each injection element to get the source's html. */
     var createApiUrl = (function () {
         var protocol = window.location.protocol === 'https:' ? 'https:' : 'http:';
